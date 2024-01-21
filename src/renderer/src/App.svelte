844cd9b60c336f9c5a81ed91ba97c41af92aeb30
Komefumi <komefumi@protonmail.ch>
2024-01-21 19:51:56 +0530

    Creates and uses modal

--- src/renderer/src/App.svelte
+++ src/renderer/src/App.svelte
@@ -1,13 +1,29 @@
 <script lang="ts">
+  import Modal from './component-lib/modal/default.svelte';
   console.log('why');
+
+  let isActive = false;
+
+  function showModal() {
+    isActive = true;
+  }
+
+  function confirm() {
+    isActive = false;
+  }
+  function cancel() {
+    isActive = false;
+  }
 </script>
+
+<Modal {isActive} {confirm} close={cancel} />
 
 <div class="columns is-mobile">
   <div class="column">One</div>
   <div class="column">Two</div>
   <div class="column">Three</div>
   <div class="column">
-    <button class="button is-primary is-fullwidth">Click</button>
+    <button on:click={showModal} class="button is-primary is-fullwidth">Click</button>
   </div>
 </div>
 
---
+++ src/renderer/src/component-lib/modal/default.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  export let isActive: boolean;
+  export let close: () => void;
+  export let confirm: () => void;
+  export let confirmText = 'Confirm';
+  export let cancelOp: () => void | undefined = undefined;
+  export let cancelText = 'Cancel';
+
+  function cancel() {
+    if (cancelOp) cancelOp();
+    close();
+  }
+</script>
+
+<div class="modal{isActive ? ' is-active' : ''}">
+  <!-- svelte-ignore a11y-click-events-have-key-events -->
+  <!-- svelte-ignore a11y-no-static-element-interactions -->
+  <div on:click={cancel} class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">Modal title</p>
+      <button on:click={cancel} class="delete" aria-label="close"></button>
+    </header>
+    <section class="modal-card-body">Modal Card Body Content</section>
+    <footer class="modal-card-foot">
+      <button on:click={confirm} class="button is-success">{confirmText}</button>
+      <button on:click={cancel} class="button">{cancelText}</button>
+    </footer>
+  </div>
+</div>
+
+<style>
+  .modal-background {
+    cursor: pointer;
+  }
+</style>
