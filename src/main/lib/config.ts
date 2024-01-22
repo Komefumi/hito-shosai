2bb60aa970468d0d302e4ce5f9a243d996176d82
Komefumi <komefumi@protonmail.ch>
2024-01-22 16:03:16 +0530

    Removes unnecessary logging

--- src/main/lib/config.ts
+++ src/main/lib/config.ts
@@ -3,4 +3,3 @@ import { app } from 'electron';
 
 export const ROOT_PATH = path.join(__dirname, '..', '..');
 export const DB_PATH = path.join(app.getPath('userData'), 'db.sqlite');
-console.log({ DB_PATH });
